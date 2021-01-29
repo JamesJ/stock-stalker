672e5ff3538d056a1e13473c8f019e1726980e85
JamesJ <contact@jamesj.me>
2021-01-29 14:56:36 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,69 @@
+console.log("Starting...")
+import discord from './discord'
+import stocks from './stock'
+
+import yargs from 'yargs'
+
+const argv = yargs
+    .option('token', {
+        alias: 't',
+        description: 'The discord token',
+        type: 'string',
+    })
+    .option('instrument', {
+        alias: 'i',
+        description: 'The instrument to use',
+        type: 'string',
+    })
+    .option('position', {
+        alias: 'p',
+        description: 'The instrument position on discord',
+        type: 'string',
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+function initDiscordConnection() {
+    discord.login(argv.token).then(value => {
+        worker(argv.instrument, argv.position)
+    })
+}
+
+initDiscordConnection()
+
+function worker(instrument, position) {
+    stocks.getPrice(instrument).then(value => {
+        const now = new Date().getMilliseconds();
+        if (value != null) {
+            discord.changeName(value, position).then(res => {
+                const finish = new Date().getMilliseconds();
+                const time = finish - now;
+
+                let delay = 1000;
+                // if the process took >1sec, lets give discord some time to breath
+                if (time > 1000) {
+                    delay = 50
+                } else {
+                    // otherwise, we'll take a 1sec delay and minus the time
+                    // this is to ensure, in theory, it gets updated every minute
+                    // rather than 1 minute from the finish of the update
+                    delay -= time
+                }
+                console.log("Took " + time + "ms to execute everything")
+                schedule(delay, instrument, position)
+            }).catch(reason => {
+                console.log(reason)
+                schedule(1000, instrument, position)
+            })
+        } else {
+            schedule(1000, instrument, position)
+        }
+    })
+}
+
+function schedule(delay, instrument, position) {
+    setTimeout(function () {
+        worker(instrument, position)
+    }, delay)
+}
---
+++ src/stock.js
@@ -0,0 +1,116 @@
+import axios from 'axios'
+
+let haltTime
+
+async function getPrice(instrument) {
+    let obj = {};
+    let res
+    try {
+        res = await getSingleStockInfo(instrument);
+    } catch (e) {
+        console.log("Failed to fetch data " + e)
+        return obj
+    }
+
+    obj = getCurrentPricingObject(res)
+    return obj
+}
+
+let lastPrice
+let closed
+function getCurrentPricingObject(instrument) {
+    const obj = {}
+    obj.name = instrument.symbol
+
+
+    if (instrument.marketState === "PRE") {
+        obj.price = instrument.preMarketPrice
+        obj.percentage = instrument.preMarketChangePercent
+    } else if (instrument.marketState === "POST") {
+        obj.price = instrument.postMarketPrice
+        obj.percentage = instrument.postMarketChangePercent
+    } else {
+        obj.price = instrument.regularMarketPrice
+        obj.percentage = instrument.regularMarketChangePercent
+        if (!instrument.tradeable && obj.price === lastPrice) {
+            //obj.halted = true
+        }
+
+        if (instrument.marketState === "POSTPOST") {
+            obj.closed = true
+            obj.lastPrice = instrument.postMarketPrice
+        }
+    }
+    if (obj.price === undefined) {
+        obj.soon = true
+        obj.text = "No data (yet)";
+        return obj
+    }
+
+
+    if (obj.price === lastPrice) {
+        obj.direction = "same"
+    } else if (obj.price < lastPrice) {
+        obj.direction = "down"
+    } else if (obj.price > lastPrice) {
+        obj.direction = "up"
+    }
+    obj.move = obj.price - lastPrice;
+    lastPrice = obj.price
+
+    let text
+
+    if (obj.price == null) {
+        text = "Failed to refresh";
+    } else {
+        if (obj.halted) {
+            text = "HALTED @ $" + obj.price.toFixed(2)
+        } else if (obj.closed) {
+            text = "Close @ $" + obj.price.toFixed(2)
+        } else {
+            text = "$" + obj.price.toFixed(2);
+
+            if (obj.closed) {
+                text += " (Closed)"
+            } else if (!isNaN(obj.move) && obj.move !== 0 && obj.move !== undefined) {
+                text += " (" + (obj.move > 0 ? "+" : "") + obj.move.toFixed(2) + ")";
+            }
+        }
+    }
+
+    obj.text = text
+
+    return obj
+}
+
+const getSingleStockInfo = stock =>
+    new Promise((resolve, reject) => {
+        if (!stock) {
+            return reject(Error('Stock symbol required'));
+        }
+        if (typeof stock !== 'string') {
+            return reject(Error(`Invalid argument type. Required: string. Found: ${typeof stock}`));
+        }
+
+        const url = `https://query1.finance.yahoo.com/v7/finance/quote?symbols=${stock}`;
+
+        return axios
+            .get(url)
+            .then((res) => {
+                const {data} = res;
+                if (
+                    !data ||
+                    !data.quoteResponse ||
+                    !data.quoteResponse.result ||
+                    data.quoteResponse.result.length === 0
+                ) {
+                    return reject(new Error(`Error retrieving info for symbol ${stock}`));
+                }
+                return resolve(data.quoteResponse.result[0]);
+            })
+            .catch(err => reject(err));
+    });
+
+export default {
+    getPrice: getPrice
+}
